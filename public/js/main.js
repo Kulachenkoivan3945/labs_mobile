63efb9adcc395d7016a5356e8685cfdff4e9b5a9
Ivan Kulachenko <kulachenkoivan@gmail.com>
2023-09-13 21:24:38 +0300

    first commit

---
+++ public/js/main.js
@@ -0,0 +1,41 @@
+const update = async() => {
+
+    let students = await fetch("/students")
+        .then(response => response.json())
+        .then(data => {
+            return data
+        }).catch(err => {
+            console.warn(err)
+            return []
+        })
+
+    return students
+}
+
+const renderStudents = (array) => {
+    let html = ""
+    for (let i in array) {
+        html +=
+            `
+        <div class="card" style="width: 100%;">
+            <div class="card-body" group=${array[i].group}>${array[i].name}</div>
+        </div>
+        `
+    }
+    // console.log(html)
+    document.getElementById("students").innerHTML = html
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("[btn]").addEventListener("click", () => {
+        update().then(data => {
+            renderStudents(data)
+            document.querySelectorAll('[group]').forEach((el) => {
+                el.addEventListener('click', (e) => {
+                    alert(`${e.target.innerHTML} ${e.target.getAttribute('group')}`);
+                })
+            })
+        })
+    })
+})
